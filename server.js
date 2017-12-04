f58029df32b43f54a8b8d13ea0c27505785ba263
Enes Emini <enes.emini@gmail.com>
2017-12-04 17:33:01 +0100

    Get and post from API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
+var io = require('socket.io')(http);
 
 app.set('port', port);
 
@@ -11,6 +13,7 @@ app.use(bodyParser.json());
 var chatHistory = [
     {
         author: "Enes Emini",
+        userId: "Enes",
         message: "Test Message retrieved from API",
         date: new Date()
     }
@@ -36,11 +39,18 @@ app.get('/posts', function(req, res, next){
 
 app.post('/posts', function(req, res, next){
     console.log(req.body);
-    chatHistory.push({message: req.body.message, author: req.body.author, date: req.body.date});
+    chatHistory.push({message: req.body.message, author: req.body.author, date: req.body.date, userId: req.body.userId});
     //res.send(chatHistory);
     res.json({message: 'API: Nachricht wurde erfolgreich erstellt.'});
 });
 
 app.listen(app.get('port'), function(){
     console.log('HSG Chat API is running...');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
