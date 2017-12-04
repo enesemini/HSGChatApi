16c138ee3f9b4e70bc51b39714175c71f9745216
Enes Emini <enes.emini@gmail.com>
2017-12-04 13:59:45 +0100

    API Update

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var chatHistory = [
-    {
-        'date': 'Mon Dec 04 2017 12:31:12 GMT+0100 (CET)',
-        'author': 'Enes Emini',
-        'message': 'Test Message 1 retrieved from API'
-    },
-    {
-        'date': 'Mon Dec 04 2017 12:31:12 GMT+0100 (CET)',
-        'author': 'Flavian',
-        'message': 'Test Message 2 retrieved from API'
-    }
+    posts: [
+        {
+            'date': 'Mon Dec 04 2017 12:31:12 GMT+0100 (CET)',
+            'author': 'Enes Emini',
+            'message': 'Test Message 1 retrieved from API'
+        },
+        {
+            'date': 'Mon Dec 04 2017 12:31:12 GMT+0100 (CET)',
+            'author': 'Flavian',
+            'message': 'Test Message 2 retrieved from API'
+        }
+    ]
 ];
 
 app.use(function(req, res, next){
